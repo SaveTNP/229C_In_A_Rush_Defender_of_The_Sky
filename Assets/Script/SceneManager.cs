6040dab73eb161439c4552bc7188c0432b611ce4
SaveTNP <73216322+SaveTNP@users.noreply.github.com>
2026-05-02 15:29:17 +0700

    Update Credit Scene

--- Assets/Script/SceneManager.cs
+++ Assets/Script/SceneManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MenuController : MonoBehaviour
 {
@@ -10,15 +11,29 @@ public class MenuController : MonoBehaviour
 
 	public void LoadMainMenu()
 	{
+		Cursor.visible = true;
 		SceneManager.LoadScene("MainMenu");
 	}
 
+	public void LoadCredit()
+	{
+		Cursor.visible = true;
+		SceneManager.LoadScene("Credit");
+	}
 	public void QuitGame()
 	{
 		Application.Quit();
 	}
 
-	public void WinScene() => SceneManager.LoadScene("Win");
-	public void LoseScene() => SceneManager.LoadScene("GameOver");
+	public void WinScene() 
+	{ 
+		Cursor.visible = true; 
+		SceneManager.LoadScene("Win"); 
+	}
+	public void LoseScene()
+	{
+		Cursor.visible = true;
+		SceneManager.LoadScene("GameOver");
+	}
 }
 
